5a6b7ef62781848a57f8e36cda8bee4c08c5959e
Circan <66139081+Circan@users.noreply.github.com>
2021-12-30 10:59:58 -0700

    Update AudioDG_Start.bat

--- AudioDG_Start.bat
+++ AudioDG_Start.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Wait for audiodg.exe to launch.
 timeout 5
-PowerShell -NoProfile -ExecutionPolicy Bypass -file C:\Users\circa\Documents\Voice_Meeter_Proc_Start.ps1
+PowerShell -NoProfile -ExecutionPolicy Bypass -file C:\Users\username\Documents\Voice_Meeter_Proc_Start.ps1
 echo Success!
 timeout 2
 exit
